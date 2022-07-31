b29f98f04fdacebbff28836b05d1b39ea27e7736
Kai Lin <kai.lin@coolabahcapital.com>
2022-08-01 00:28:20 +0100

    use_cached: handle different function enclosing environments

--- tests/testthat/test_use_cached.R
+++ tests/testthat/test_use_cached.R
@@ -1,6 +1,7 @@
 
 test_that("use cached", {
 
+  # check cached versions get used, and environments are separated
   modules::invalidate_cache()
   expect_output(mod_b <- modules::use_cached(test_path('mod_b.R')), '\\[1\\] "in mod a"\n\\[1\\] "in mod b"')
   expect_output(mod_a <- modules::use_cached(test_path('mod_a.R')), NA)
@@ -25,5 +26,16 @@ test_that("use cached", {
   mod_a$set_module_setting(3)
   expect_equal(mod_b$get_module_a_setting(), 2)
   expect_equal(mod_a$get_module_setting(), 3)
+
+  # check handling of different enclosing environments
+  modules::invalidate_cache()
+  mod_a <- modules::use_cached(test_path('mod_a.R'))
+  mod_a_cached <- modules::use_cached(test_path('mod_a.R'))
+
+  expect_equal(mod_a$func_mean(10), 10)
+  expect_equal(mod_a_cached$func_mean(10), 10)
+
+  expect_true(get_env_id(environment(mod_a$make_func)) == get_env_id(parent.env(environment(mod_a$func_mean))))
+  expect_true(get_env_id(environment(mod_a_cached$make_func)) == get_env_id(parent.env(environment(mod_a_cached$func_mean))))
 
 })
